1a7b512bd8b21acf21c0b8686cc63f56660d74e4
Lesley Chanzu <lesleychanzu3@gmail.com>
2025-04-15 01:27:52 +0300

    changed background-color for the screen

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   }
 
   return (
-    <div className='w-full h-screen grid place-items-center bg-orange-50'>
+    <div className='w-full h-screen grid place-items-center bg-orange-100'>
       <div className={`${frameZoom && "min-w-[97vw] min-h-[97vh]"} w-[70vw] h-[85vh] min-w-[70vw] min-h-[85vh] max-w-[90vw] max-h-[90vh] border border-gray-300 rounded-2xl resize overflow-auto relative transition-all duration-100 border-shadow-lg shadow-lg bg-white`}>
 
         <Navbar />
