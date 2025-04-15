66c541db217b3d736f0205ec643f1d60317be1c2
Lesley Chanzu <lesleychanzu3@gmail.com>
2025-04-15 20:31:18 +0300

    added navbar and social media icons

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,11 @@ import Control from './components/control';
 import Navbar from './components/Navbar';
 function App() {
   const [frameZoom, setFrameZoom] = useState(false);
+  const [activePage, setActivePage] = useState(0);
+
+  const handleNavClick = (pageIndex) => {
+    setActivePage(pageIndex);
+  }
 
   const toggleZoom = () => {
     setFrameZoom(!frameZoom);
@@ -12,7 +17,7 @@ function App() {
     <div className='w-full h-screen grid place-items-center bg-orange-100'>
       <div className={`${frameZoom && "min-w-[97vw] min-h-[97vh]"} w-[70vw] h-[85vh] min-w-[70vw] min-h-[85vh] max-w-[90vw] max-h-[90vh] border border-gray-300 rounded-2xl resize overflow-auto relative transition-all duration-100 border-shadow-lg shadow-lg bg-white`}>
 
-        <Navbar />
+        <Navbar activePage={activePage} handleNavClick={handleNavClick}/>
         <Control  toggleZoom={toggleZoom} frameZoom={frameZoom}/>
       </div>
     </div>
